b76ebf7a5c384744d617832d86f8a05900b32b2d
Brian Dunnette <dunn0172@umn.edu>
2016-11-17 15:59:23 -0600

    basic sign in/out dialog

--- public/js/app.js
+++ public/js/app.js
@@ -92,116 +92,147 @@ app.controller('NavbarCtrl', function($scope, $firebaseAuth) {
 });
 
 app.controller('AttendanceCtrl', function($scope, $firebaseArray, $mdDialog) {
-    var storageRef = firebase.storage().ref().child("images");
     var ref = firebase.database().ref().child("participants");
     $scope.participants = $firebaseArray(ref);
-    $scope.editingImage = {};
 
-    var _video = null,
-        patData = null;
-
-    $scope.patOpts = {
-        x: 0,
-        y: 0,
-        w: 25,
-        h: 25
-    };
-
-    // Setup a channel to receive a video property
-    // with a reference to the video element
-    // See the HTML binding in main.html
-    $scope.channel = {
-        videoWidth: 160,
-        videoHeight: 120
-    };
-
-    $scope.webcamError = false;
-    $scope.onError = function(err) {
-        $scope.$apply(
-            function() {
-                $scope.webcamError = err;
-            }
-        );
-    };
-
-    $scope.onSuccess = function() {
-        // The video element contains the captured camera data
-        _video = $scope.channel.video;
-        $scope.$apply(function() {
-            $scope.patOpts.w = _video.width;
-            $scope.patOpts.h = _video.height;
-            $scope.showDemos = true;
-        });
-    };
-
-    $scope.onStream = function(stream) {
-        // You could do something manually with the stream.
-    };
-
-
-    /**
-     * Make a snapshot of the camera data and show it in another canvas.
-     */
-    $scope.makeSnapshot = function makeSnapshot() {
-        if (_video) {
-            var patCanvas = document.querySelector('#snapshot');
-            if (!patCanvas) return;
-
-            patCanvas.width = _video.width;
-            patCanvas.height = _video.height;
-            var ctxPat = patCanvas.getContext('2d');
-
-            var idata = getVideoData($scope.patOpts.x, $scope.patOpts.y, $scope.patOpts.w, $scope.patOpts.h);
-            ctxPat.putImageData(idata, 0, 0);
-            console.log(patCanvas);
-            patCanvas.toBlob(function(blob) {
-                var imageName = Date.now().toString();
-                console.log(imageName);
-                var uploadTask = storageRef.child(imageName + '.png').put(blob);
-                uploadTask.on('state_changed', function(snapshot) {
-                    // Observe state change events such as progress, pause, and resume
-                    // See below for more detail
-                }, function(error) {
-                    // Handle unsuccessful uploads
-                    console.error(error);
-                }, function() {
-                    // Handle successful uploads on complete
-                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-                    var downloadURL = uploadTask.snapshot.downloadURL;
-                    console.log(uploadTask.snapshot);
-                    $scope.editingImage.url = downloadURL;
-                    console.log($scope.editingImage);
-                });
+    $scope.showDialog = function(ev) {
+        $mdDialog.show({
+                controller: SignInOutController,
+                templateUrl: 'views/sign-in-out.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: true
             })
-
-            patData = idata;
-        }
+            .then(function(answer) {
+                console.log(answer);
+            }, function() {
+                console.log('You cancelled the dialog.');
+            });
     };
 
-    /**
-     * Redirect the browser to the URL given.
-     * Used to download the image by passing a dataURL string
-     */
-    $scope.downloadSnapshot = function downloadSnapshot(dataURL) {
-        window.location.href = dataURL;
-    };
+    function SignInOutController($scope, $mdDialog) {
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
 
-    var getVideoData = function getVideoData(x, y, w, h) {
-        var hiddenCanvas = document.createElement('canvas');
-        hiddenCanvas.width = _video.width;
-        hiddenCanvas.height = _video.height;
-        var ctx = hiddenCanvas.getContext('2d');
-        ctx.drawImage(_video, 0, 0, _video.width, _video.height);
-        return ctx.getImageData(x, y, w, h);
-    };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
 
-    /**
-     * This function could be used to send the image data
-     * to a backend server that expects base64 encoded images.
-     *
-     * In this example, we simply store it in the scope for display.
-     */
-    var sendSnapshotToServer = function sendSnapshotToServer(imgBase64) {
-        $scope.snapshotData = imgBase64;
-    };
+        $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+        };
+
+        $scope.editingImage = {};
+        var storageRef = firebase.storage().ref().child("images");
+
+        var _video = null,
+            patData = null;
+
+        $scope.patOpts = {
+            x: 0,
+            y: 0,
+            w: 25,
+            h: 25
+        };
+
+        // Setup a channel to receive a video property
+        // with a reference to the video element
+        // See the HTML binding in main.html
+        $scope.channel = {
+            videoWidth: 160,
+            videoHeight: 120
+        };
+
+        $scope.webcamError = false;
+        $scope.onError = function(err) {
+            $scope.$apply(
+                function() {
+                    $scope.webcamError = err;
+                }
+            );
+        };
+
+        $scope.onSuccess = function() {
+            // The video element contains the captured camera data
+            _video = $scope.channel.video;
+            $scope.$apply(function() {
+                $scope.patOpts.w = _video.width;
+                $scope.patOpts.h = _video.height;
+                $scope.showDemos = true;
+            });
+        };
+
+        $scope.onStream = function(stream) {
+            // You could do something manually with the stream.
+        };
+
+
+        /**
+         * Make a snapshot of the camera data and show it in another canvas.
+         */
+        $scope.makeSnapshot = function makeSnapshot() {
+            if (_video) {
+                var patCanvas = document.querySelector('#snapshot');
+                if (!patCanvas) return;
+
+                patCanvas.width = _video.width;
+                patCanvas.height = _video.height;
+                var ctxPat = patCanvas.getContext('2d');
+
+                var idata = getVideoData($scope.patOpts.x, $scope.patOpts.y, $scope.patOpts.w, $scope.patOpts.h);
+                ctxPat.putImageData(idata, 0, 0);
+                console.log(patCanvas);
+                patCanvas.toBlob(function(blob) {
+                    var imageName = Date.now().toString();
+                    console.log(imageName);
+                    var uploadTask = storageRef.child(imageName + '.png').put(blob);
+                    uploadTask.on('state_changed', function(snapshot) {
+                        // Observe state change events such as progress, pause, and resume
+                        // See below for more detail
+                    }, function(error) {
+                        // Handle unsuccessful uploads
+                        console.error(error);
+                    }, function() {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        var downloadURL = uploadTask.snapshot.downloadURL;
+                        console.log(uploadTask.snapshot);
+                        $scope.editingImage.url = downloadURL;
+                        console.log($scope.editingImage);
+                    });
+                })
+
+                patData = idata;
+            }
+        };
+
+        /**
+         * Redirect the browser to the URL given.
+         * Used to download the image by passing a dataURL string
+         */
+        $scope.downloadSnapshot = function downloadSnapshot(dataURL) {
+            window.location.href = dataURL;
+        };
+
+        var getVideoData = function getVideoData(x, y, w, h) {
+            var hiddenCanvas = document.createElement('canvas');
+            hiddenCanvas.width = _video.width;
+            hiddenCanvas.height = _video.height;
+            var ctx = hiddenCanvas.getContext('2d');
+            ctx.drawImage(_video, 0, 0, _video.width, _video.height);
+            return ctx.getImageData(x, y, w, h);
+        };
+
+        /**
+         * This function could be used to send the image data
+         * to a backend server that expects base64 encoded images.
+         *
+         * In this example, we simply store it in the scope for display.
+         */
+        var sendSnapshotToServer = function sendSnapshotToServer(imgBase64) {
+            $scope.snapshotData = imgBase64;
+        };
+    }
 });
